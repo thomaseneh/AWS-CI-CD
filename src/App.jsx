580aee752a1bca614cbc7e47a8461d11823a324f
Thomas Eneh <thomaseneh@yahoo.com>
2023-12-11 02:38:33 -0600

    React-form

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header.jsx";
+import Signup from "./components/signupForm/Signup.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Signup />
+    </>
+  );
+}
+
+export default App;
